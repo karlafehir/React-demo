16711e6f09dc7237e5dcba7d1bb8e6094b9f159b
karlafehir <fehirkarla@gmail.com>
2025-03-03 17:05:19 +0100

    passing children to a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
 
   return (
     <div>
+      <Alert>
+        This is an <strong>Alert</strong>
+      </Alert>
       <ListGroup items={items} heading="Cities" onSelectItem={onSelectItem} />
     </div>
   );
